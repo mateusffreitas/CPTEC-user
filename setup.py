15df68c7339720bfe8374aabc515206ea736a5b1
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2023-01-09 13:30:49 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='SetupFile',
+    version='1.0',
+    scripts=['cptecmodel/CPTEC_BAM.py','cptecmodel/CPTEC_ETA.py','cptecmodel/CPTEC_GFS.py',
+    			'cptecmodel/CPTEC_UTIL.py','cptecmodel/CPTEC_Widgets.py','cptecmodel/CPTEC_WRF.py'],
+)
